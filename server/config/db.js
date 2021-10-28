bb4013b54f3be9544cee6664e5e2a4697f698d47
lkb0398 <lkb0398@nate.com>
2021-10-28 08:58:42 -0400

    comp229-f2021-midterm-301083034

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://kb:3UMISXzzHPkrUrEE@midtermdbserver.1xtvy.mongodb.net/books229?retryWrites=true&w=majority"
+};
